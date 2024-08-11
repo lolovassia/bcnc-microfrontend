10a1a991934a35bd4e66d0961725d80cc1129387
rodrigovassia <rodrigovassia@ipglobal.es>
2024-08-11 23:54:21 +0200

    Feature: create boilerplate of the USERS project

---
+++ users/src/App.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+
+import React from 'react';
+
+import './index.css';
+
+const App = () => <div>User - Boilerplate</div>;
+const rootElement = document.getElementById('app');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(<App />);
